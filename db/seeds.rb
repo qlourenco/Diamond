dfc7768fbad91424f16b38b55f3da6b69caed905
terence truong <tvn.terence@gmail.com>
2021-12-01 10:45:59 +0100

    seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -157,7 +157,7 @@ Favorite.create(user: emile, vinyl: it_was_written)
 Favorite.create(user: emile, vinyl: ego_trip)
 
 puts "[TAGS] ..."
-TAGS = ["1950", "1960", "1970", "1980", "1990", "2000", "2010", "2020", "Dirthdays", "Summer", "Winter", "Spring", "Autumn", "Favorites", "Travel", "Calm", "Party", "Family", "Bad mood", "Happy", "Other", "To sell", "Dj", "Dance", "Sing", "Beats", "Live", "Band", "Girl", "Top", "Hit", "Popular", "Rave", "Bass", "Drum", "Cool", "French", "German", "Hispanic", "English", "US", "Russian", "Oriental", "Asian", "African", "European", "American", "Tribal", "Chill", "Hard", "Sport", "Nostalgia", "Childhood", "Rain", "Training", "Studying"]
+TAGS = ["1950", "1960", "1970", "1980", "1990", "2000", "2010", "2020", "Birthdays", "Summer", "Winter", "Spring", "Autumn", "Favorites", "Travel", "Calm", "Party", "Family", "Bad mood", "Happy", "Other", "To sell", "Dj", "Dance", "Sing", "Beats", "Live", "Band", "Girl", "Top", "Hit", "Popular", "Rave", "Bass", "Drum", "Cool", "French", "German", "Hispanic", "English", "US", "Russian", "Oriental", "Asian", "African", "European", "American", "Tribal", "Chill", "Hard", "Sport", "Nostalgia", "Childhood", "Rain", "Training", "Studying"]
 
 puts "[TRACKS] ..."
 # TODO: Tracks + add in Vinyl
