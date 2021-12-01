7de47b98585e7b7f043910361bdaf96b8658ea65
Clement Le Boulanger <hello@clement-leboulanger.com>
2021-12-01 15:29:07 +0100

    AddHomeUserFrontEnd

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if current_user
+      render 'home_user'
+    else
+      render "home"
+    end
   end
 end
