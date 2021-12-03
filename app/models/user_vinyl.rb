dd7358833c3a9dc05ef0c31d6d343b9e98f82831
Clement Le Boulanger <hello@clement-leboulanger.com>
2021-12-03 15:15:40 +0100

    Fix-Destroy-conflict

--- app/models/user_vinyl.rb
+++ app/models/user_vinyl.rb
@@ -3,5 +3,5 @@ class UserVinyl < ApplicationRecord
   belongs_to :user
 
   has_many :vinyl_tags
-  has_many :tags, through: :vinyl_tags
+  has_many :tags, through: :vinyl_tags, dependent: :destroy
 end
