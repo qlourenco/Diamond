9e3e0f846054d5443681b74a7cab190caa0fe390
Clement Le Boulanger <hello@clement-leboulanger.com>
2021-12-07 15:00:19 +0100

    Refont_Btn_Google_&_Facebook

---
+++ app/javascript/components/particule.js
@@ -0,0 +1,16 @@
+// const particules = () => {
+//   const counts = document.querySelectorAll('.count')
+
+//   function getRandomInt(max) {
+//     return Math.floor(Math.random() * max);
+//   }
+
+//   counts.forEach(count => {
+//     let fromTop = getRandomInt(203/1.2)
+//     let fromLeft = getRandomInt(375/2)
+//     count.style.top = `${fromTop}px`
+//     count.style.left = `${fromLeft}px`
+//   });
+// }
+
+export { particules };
