d0ee7ff4e39cb813ccea29ad7c37173220d39651
Mathias Roux <mathiasroux.pro@gmail.com>
2021-12-09 09:46:13 +0100

    Fixed User favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,25 +1,27 @@
 class FavoritesController < ApplicationController
   def index
+    @user = current_user
     if params[:query].present?
       sql_query = "title ILIKE :query OR name ILIKE :query"
-      @favorites = Favorite.joins(vinyl: :artist).where(sql_query, query: "%#{params[:query]}%")
+      @favorites = @user.favorites.joins(vinyl: :artist).where(sql_query, query: "%#{params[:query]}%")
     else
-      @favorites = Favorite.all
+      @favorites = @user.favorites.all
     end
   end
 
   def create_from_favorite
     @favorite = Favorite.find(params[:id])
     @user = current_user
-    @user_vinyl = UserVinyl.create(vinyl_id: @favorite[:vinyl_id], user: @user)
+    @user_vinyl = UserVinyl.create(user: @user, vinyl_id: @favorite[:vinyl_id])
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
     redirect_to user_vinyl_path(@user_vinyl)
   end
 
   def create
+    @user = current_user
     @vinyl = Vinyl.find(params[:vinyl_id])
-    @favorite = Favorite.create(vinyl: @vinyl, user: current_user)
+    @favorite = Favorite.create(user: @user, vinyl: @vinyl)
     redirect_to vinyl_path(@vinyl)
   end
 
