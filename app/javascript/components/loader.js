e1a7077aee0b7b10145799a5a18d42ac3e4f4af2
terence truong <tvn.terence@gmail.com>
2021-12-09 10:20:38 +0100

    Take off JS errors

--- app/javascript/components/loader.js
+++ app/javascript/components/loader.js
@@ -1,10 +1,12 @@
-
 const loaderSearch = () => {
-  // const btnSearch = document.getElementById('btn-search')
-  // const loader = document.querySelector('.content-loader')
-  // btnSearch.addEventListener('click', () => {
-  //   loader.style.display = 'flex'
-  // })
+  const btnSearch = document.getElementById('btn-search')
+  const loader = document.querySelector('.content-loader')
+  if (btnSearch == null) {
+    return
+  }
+  btnSearch.addEventListener('click', () => {
+    loader.style.display = 'flex'
+  })
 }
 
 export { loaderSearch };
