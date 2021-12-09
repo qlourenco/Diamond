17b6083766e3a4041065f8ce682cb76cee56a094
Clement Le Boulanger <hello@clement-leboulanger.com>
2021-12-09 11:47:58 +0100

    RefactoFrontEver

--- app/controllers/user_vinyls_controller.rb
+++ app/controllers/user_vinyls_controller.rb
@@ -13,7 +13,9 @@ class UserVinylsController < ApplicationController
       arr << vinyl.vinyl.genre_id
     end
     @hash_vinyl = arr.group_by { |i| i }
-
+    @first_genre = @hash_vinyl.values[0]
+    @second_genre = @hash_vinyl.values[1]
+    @third_genre = @hash_vinyl.values[2]
   end
 
   def create
