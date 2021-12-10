d01781a96b748de2e966e760b6b5ebe8de5cf614
terence truong <tvn.terence@gmail.com>
2021-12-10 11:01:43 +0100

    center and anim on + button

--- db/seeds.rb
+++ db/seeds.rb
@@ -155,7 +155,6 @@
 # Favorite.create(user: emile, vinyl: the_now_now)
 # Favorite.create(user: emile, vinyl: it_was_written)
 # Favorite.create(user: emile, vinyl: ego_trip)
-=======
 
 
 # puts "[TAGS] ..."
